ca38c7869737b9a509e6255a41ed5e2a29652e5a
reizen-desu <nokiaq80@gmail.com>
2022-11-30 15:51:29 +0200

    style: formatting

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,101 +6,94 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
-use Hash;
+use Illuminate\Support\Facades\Hash;
+
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name'=>'required|min:2|max:100',
-            'email'=>'required|email|unique:users',
-            'password'=>'required|min:6|max:100',
-            'confirm_password'=>'required|same:password'
+            'name' => 'required|min:2|max:100',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|max:100',
+            'confirm_password' => 'required|same:password'
         ]);
 
         if ($validator->fails()) {
             return response()->json([
-                'message'=>'Falha na validação',
-                'errors'=>$validator->errors()
-            ],422);
+                'message' => 'Falha na validação',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>Hash::make($request->password)
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
         ]);
 
 
         return response()->json([
-            'message'=>'Registro bem-sucedido',
-            'data'=>$user
-        ],200);
-
-
-
+            'message' => 'Registro bem-sucedido',
+            'data' => $user
+        ], 200);
     }
 
 
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $validator = Validator::make($request->all(), [
-            'email'=>'required|email',
-            'password'=>'required',
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
 
         if ($validator->fails()) {
             return response()->json([
-                'message'=>'Falha na validação',
-                'errors'=>$validator->errors()
-            ],422);
+                'message' => 'Falha na validação',
+                'errors' => $validator->errors()
+            ], 422);
         }
 
-        $user=User::where('email',$request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-        if($user){
+        if ($user) {
 
-            if(Hash::check($request->password,$user->password)){
+            if (Hash::check($request->password, $user->password)) {
 
-                $token=$user->createToken('auth-token')->plainTextToken;
+                $token = $user->createToken('auth-token')->plainTextToken;
 
                 return response()->json([
-                    'message'=>'Logado com Sucesso!!!',
-                    'token'=>$token,
-                    'data'=>$user
-                ],200); 
-
-            }else{
+                    'message' => 'Logado com Sucesso!!!',
+                    'token' => $token,
+                    'data' => $user
+                ], 200);
+            } else {
                 return response()->json([
-                    'message'=>'Credenciais incorretas',
-                ],400); 
+                    'message' => 'Credenciais incorretas',
+                ], 400);
             }
-
-        }else{
+        } else {
 
             return response()->json([
-                'message'=>'Credenciais incorretas',
-            ],400); 
+                'message' => 'Credenciais incorretas',
+            ], 400);
         }
-
-
-
-
     }
 
-    public function user(Request $request){
+    public function user(Request $request)
+    {
         return response()->json([
-            'message'=>'Usuário obtido com sucesso',
-            'data'=>$request->user()
-        ],200); 
+            'message' => 'Usuário obtido com sucesso',
+            'data' => $request->user()
+        ], 200);
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
 
-       $request->user()->currentAccessToken()->delete(); 
+        $request->user()->currentAccessToken()->delete();
         return response()->json([
-            'message'=>'Usuário desconectado com sucesso, Volte Sempre',
-        ],200); 
+            'message' => 'Usuário desconectado com sucesso, Volte Sempre',
+        ], 200);
     }
-
-
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('district');
-            $table->string('city');
-            $table->string('province');
+            $table->string('district')->nullable;
+            $table->string('city')->nullable;
+            $table->string('province')->nullable;
             $table->string('cellphone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
