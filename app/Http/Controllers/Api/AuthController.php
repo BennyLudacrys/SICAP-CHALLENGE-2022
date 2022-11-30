d77a143d9e9f10e39eb83a38c8ba4fc0d6b123a9
reizen-desu <nokiaq80@gmail.com>
2022-11-30 15:50:57 +0200

    refactor: remove unused comented code

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,14 +9,8 @@ use App\Models\User;
 use Hash;
 class AuthController extends Controller
 {
-    public function register(Request $request){
-        // $request->validate([
-        //     'name'=>'required|min:2|max:100',
-        //     'email'=>'required|email|unique:users',
-        //     'password'=>'required|min:6|max:100',
-        //     'confirm_password'=>'required|same:password'
-        // ]);
-
+    public function register(Request $request)
+    {
         $validator = Validator::make($request->all(), [
             'name'=>'required|min:2|max:100',
             'email'=>'required|email|unique:users',
